91794b2ef4ec7cfcf135bfc394979adc4bb4a2e5
Roger(Xingyun) Chen <44486666+chemxy@users.noreply.github.com>
2024-08-18 23:05:45 -0700

    add progress bar

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -6,11 +6,16 @@ import {useEffect, useState} from "react";
 export default function Dashboard() {
 
     const [dashboardStats, setDashboardStats] = useState({
+        allTasks: 0,
         newTasks: 0,
         inProgressTasks: 0,
         doneTasks: 0,
         tasksDueToday: 0,
         taskDueTomorrow: 0
+    })
+
+    const [circle, setCircle] = useState({
+        diameter: 200
     })
 
     useEffect(() => {
@@ -20,7 +25,10 @@ export default function Dashboard() {
             // console.log(resData)
             setDashboardStats({
                 ...dashboardStats,
-                newTasks: resData.new,
+                // allTasks: resData.all,
+                allTasks: 4,
+                // newTasks: resData.new,
+                newTasks: 2,
                 inProgressTasks: resData.inProgress,
                 doneTasks: resData.done,
                 tasksDueToday: resData.dueToday,
@@ -33,6 +41,21 @@ export default function Dashboard() {
     return (
         <div>
             <div className="flex-row dashboard-wrapper">
+                <div>
+                    <svg width={circle.diameter * 2} height={circle.diameter * 2}>
+                        <circle cx={circle.diameter} cy={circle.diameter} r={circle.diameter / 2} fill="none"
+                                stroke="lightgrey"
+                                strokeWidth={circle.diameter / 5}/>
+                        <circle cx={circle.diameter} cy={circle.diameter} r={circle.diameter / 2} fill="none"
+                                stroke="green"
+                                strokeWidth={circle.diameter / 5}
+                                strokeDasharray={`${dashboardStats.newTasks / dashboardStats.allTasks * 618},99999`} strokeLinecap={"round"}
+                                transform={`rotate(-90, ${circle.diameter}, ${circle.diameter})`}/>
+                        <text x={circle.diameter * 0.905}
+                              y={circle.diameter * 1.05}>{dashboardStats.newTasks / dashboardStats.allTasks * 100} %
+                        </text>
+                    </svg>
+                </div>
                 <DashboardSmallCell value={dashboardStats.newTasks} title={"new task"}></DashboardSmallCell>
                 <DashboardSmallCell value={dashboardStats.inProgressTasks} title={"in progress"}></DashboardSmallCell>
                 <DashboardSmallCell value={dashboardStats.doneTasks} title={"task done"}></DashboardSmallCell>
@@ -42,6 +65,7 @@ export default function Dashboard() {
                 <DashboardLargeCell value={dashboardStats.taskDueTomorrow}
                                     title={"tasks due tomorrow"}></DashboardLargeCell>
             </div>
+
         </div>
 
     );
